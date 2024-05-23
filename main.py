87bdd0f161325c7084eea60df794da3c388afe45
Ben <ben@swit.io>
2024-05-23 17:46:58 +0900

    feat(langchain): add openai langchain

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from langchain import
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv(".env", verbose=True)
+
+
+
+llm = OpenAI()
+
+result = llm.invoke("안녕?")
+print(result)
